fad35a580ddf1b4fdf0848a43d9c0c68dfa3a6f1
Mrmagicpie <realmrmagicpie@gmail.com>
2021-02-23 16:32:35 -0500

    Removing imports

--- PieCodeV2/PieCodeV2/Interpreter/Commands.cs
+++ PieCodeV2/PieCodeV2/Interpreter/Commands.cs
@@ -4,12 +4,6 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PieCodeV2.Interpreter
 {
--- PieCodeV2/PieCodeV2/Interpreter/Interpreter.cs
+++ PieCodeV2/PieCodeV2/Interpreter/Interpreter.cs
@@ -6,9 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection.Metadata.Ecma335;
 using System.Security;
-using System.Text;
 using System.Threading.Tasks;
 using PieCodeV2.Errors;
 
