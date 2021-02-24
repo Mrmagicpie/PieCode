5996e3c60fbff46dfeff8d8a670d5299b0394d9f
Mrmagicpie <realmrmagicpie@gmail.com>
2021-02-24 11:43:34 -0500

    Update Interpreter.cs

--- PieCodeV2/PieCodeV2/Interpreter/Interpreter.cs
+++ PieCodeV2/PieCodeV2/Interpreter/Interpreter.cs
@@ -42,16 +42,20 @@ namespace PieCodeV2.Interpreter
         private static async Task Interprete(InterpreterFile interpreterFile, string file)
         {
             try
-            { interpreterFile.SetFile(file); }
+            {
+                interpreterFile.SetFile(file);
+            }
             catch (FileNotValid)
             {
                 Console.WriteLine("\nProgramming Error:");
                 Console.WriteLine("You can only use a .pie file!\n");
                 return;
             }
-            
+
             try
-            { interpreterFile.contents = File.ReadAllLines(interpreterFile.file); }
+            {
+                interpreterFile.contents = File.ReadAllLines(interpreterFile.file);
+            }
             catch (SecurityException)
             {
                 Console.WriteLine("\nSystem Error:");
@@ -59,74 +63,75 @@ namespace PieCodeV2.Interpreter
                 return;
             }
 
-            int line       = 0;
+            int line = 0;
             // char[] split   = { ' ', '<', '>' };
             // char[] split   = {' '};
             // string last_func;
-            
+
             foreach (var lines in interpreterFile.contents)
             {
                 // Console.WriteLine(lines);
-                
+
                 // Set things to ignore while running the program.
                 if (
-                    string.IsNullOrWhiteSpace(lines) ||
-                    string.IsNullOrWhiteSpace(lines) ||
-                    lines.StartsWith(";")
-                    ) 
-                { continue; }
+                    string.IsNullOrWhiteSpace(lines)      ||
+                    string.IsNullOrWhiteSpace(lines)      ||
+                    lines.StartsWith(";")                 ||
+                    (line == 0 && lines.StartsWith("#!"))
+                )
+                { line++; continue; }
 
                 try
                 {
-                    if (!(line == 0 && lines.StartsWith("#!")))
+                    string temp_lines;
+
+                    if (!lines.EndsWith(" "))
+                    { temp_lines = lines + " "; }
+                    else
+                    { temp_lines = lines; }
+
+                    string[] temp_line;
+
+                    temp_line = temp_lines.Split("<", 2);
+                    // temp_line                 =  temp_lines.Split(split, 2);
+                    // object[] parameters       =  { temp_line[1] };
+
+                    // I forgot why I did this but leaving it commented in case.
+                    // int param_count = command.GetParameters().Length;
+                    // foreach (var par in command_par)
+                    // {
+                    //     if (par.IsOptional)
+                    //     {
+                    //         param_count--;
+                    //     }
+                    // }
+
+                    // TODO: Make this actually be able to process not only the first thing
+                    MethodInfo command       =  typeof(Commands.Commands).GetMethod(temp_line[0]);
+                    List<object> parameters  =  new List<object>();
+                    int para_int             =  0;
+
+                    foreach (var parameter in command.GetParameters())
                     {
-                        string    temp_lines;
-
-                        if (!lines.EndsWith(" ")) {temp_lines = lines + " ";}
-                        else { temp_lines = lines; }
-                        
-                        string[]  temp_line;
-
-                        temp_line                    = temp_lines.Split("<", 2);
-                        // temp_line                 =  temp_lines.Split(split, 2);
-                        // object[] parameters       =  { temp_line[1] };
-
-                        // TODO: Make this actually be able to process not only the first thing
-                        MethodInfo command           = typeof(Commands.Commands).GetMethod(temp_line[0]);
-                        
-                        // I forgot why I did this but leaving it commented in case.
-                        // int param_count = command.GetParameters().Length;
-                        // foreach (var par in command_par)
-                        // {
-                        //     if (par.IsOptional)
-                        //     {
-                        //         param_count--;
-                        //     }
-                        // }
-                        
-                        List<object> parameters      = new List<object>();
-                        int para_int                 = 0;
-                        
-                        foreach (var parameter in command.GetParameters())
+                        try
                         {
-                            try
-                            { parameters.Add(temp_line[para_int + 1]); para_int++; }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine("Syntax error:\n Mismatch arguments!");
-                                return;
-                                // throw new Exception();
-                            }
+                            parameters.Add(temp_line[para_int + 1]);
+                            para_int++;
                         }
-                        
-                        object[] new_para = parameters.ToArray();
-                        command.Invoke(null, new_para);
-
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Syntax error:\n Mismatch arguments!");
+                            return;
+                            // throw new Exception();
+                        }
                     }
-                    
+
+                    object[] new_para = parameters.ToArray();
+                    command.Invoke(null, new_para);
+
                     line++;
                 }
-                
+
                 catch (NullReferenceException)
                 {
                     Console.WriteLine("\nSyntax error at line {0}: function not found", line + 1);
